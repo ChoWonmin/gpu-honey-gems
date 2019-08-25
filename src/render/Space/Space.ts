9afac6e47553708cfc84145771cca3615f159694
ChoWonmin <0108257@gmail.com>
2019-08-25 15:39:38 +0900

    create star

--- src/render/Space/Space.ts
+++ src/render/Space/Space.ts
@@ -11,8 +11,17 @@ export default class Space extends Vue {
   private scene: any = null;
   private renderer: any = null;
 
+  // private width: number = 0;
+  // private height: number = 0;
+
+  private stars: THREE.Mesh[] = [];
+  private length: number = 100;
+
   private init() {
     const container = document.getElementById('container') as HTMLElement;
+
+    // this.width = container.clientWidth;
+    // this.height = container.clientHeight;
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
@@ -21,6 +30,19 @@ export default class Space extends Vue {
       0.1,
       1000,
     );
+    this.camera.position.z = 150;
+
+    for (let i = 0; i < this.length; i++) {
+      const geometry = new THREE.CircleGeometry(2, 24);
+      const material = new THREE.MeshBasicMaterial({ color: 0xefefef });
+
+      const star = new THREE.Mesh(geometry, material);
+      star.position.x =
+        Math.random() * container.clientWidth - container.clientWidth / 2;
+      // this.stars.push(star);
+      this.scene.add(star);
+    }
+
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
     });
@@ -32,7 +54,13 @@ export default class Space extends Vue {
     }
   }
 
+  private animate() {
+    requestAnimationFrame(this.animate);
+    this.renderer.render(this.scene, this.camera);
+  }
+
   private mounted() {
     this.init();
+    this.animate();
   }
 }
