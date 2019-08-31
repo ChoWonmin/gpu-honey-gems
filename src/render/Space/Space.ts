ff5eea151e091f5ef769ce93e90f8cd57f616d0e
ChoWonmin <0108257@gmail.com>
2019-08-31 23:54:32 +0900

    draw stars

--- src/render/Space/Space.ts
+++ src/render/Space/Space.ts
@@ -15,31 +15,31 @@ export default class Space extends Vue {
   // private height: number = 0;
 
   private stars: THREE.Mesh[] = [];
-  private length: number = 100;
+  private starsLen: number = 200;
 
   private init() {
     const container = document.getElementById('container') as HTMLElement;
 
-    // this.width = container.clientWidth;
-    // this.height = container.clientHeight;
+    this.width = container.clientWidth;
+    this.height = container.clientHeight;
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
       75,
-      container.clientWidth / container.clientHeight,
+      this.width / this.height,
       0.1,
       1000,
     );
     this.camera.position.z = 150;
 
-    for (let i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.starsLen; i++) {
       const geometry = new THREE.CircleGeometry(2, 24);
       const material = new THREE.MeshBasicMaterial({ color: 0xefefef });
 
       const star = new THREE.Mesh(geometry, material);
-      star.position.x =
-        Math.random() * container.clientWidth - container.clientWidth / 2;
-      // this.stars.push(star);
+      star.position.x = Math.random() * this.width - this.width / 2;
+      star.position.y = Math.random() * this.height - this.height / 2;
+      this.stars.push(star);
       this.scene.add(star);
     }
 
@@ -47,7 +47,7 @@ export default class Space extends Vue {
       antialias: true,
     });
 
-    this.renderer.setSize(container.clientWidth, container.clientHeight);
+    this.renderer.setSize(this.width, this.height);
 
     if (container) {
       container.appendChild(this.renderer.domElement);
@@ -56,6 +56,11 @@ export default class Space extends Vue {
 
   private animate() {
     requestAnimationFrame(this.animate);
+
+    for (let i = 0; i < this.stars.length; i++) {
+      this.stars[i].position.z -= 0.1;
+    }
+
     this.renderer.render(this.scene, this.camera);
   }
 
